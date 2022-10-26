7c31f9c497bfbb32d361c323b0de4bc2a82abb7d
Abhiramreddy Pulakurthy <v5700145@ness.com>
2022-10-26 00:20:56 -0400

    version change

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping(path = "/")
     public String getVersion() {
-        return "Version: 2";
+        return "Version: 3";
     }
 }
